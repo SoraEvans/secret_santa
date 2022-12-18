ede55ec339f72f9045b1cb5cbe34b7d6d271b9db
Alexander Potikhonov <pas1996@mail.ru>
2022-12-18 21:49:05 +0300

    merge footer & instructions

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
-
+import Cont from "./style_cont";
 
 const InstrSec = styled.section`
 background: #FFFFFF;
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,20 +4,21 @@ import Header from '../components/header'
 import Main from '../components/main'
 import Instr from '../components/instructions'
 // дефолтное поэтому импорт без фигурных скобок
-
 import Footer from '../components/footer'
 import MkBoxBtn from '../components/mkbox_btn'
 import Carousel from '../components/Carousel'
+import AccordionFaq from '../components/AccordionFaq'
 
 const Home = () => (
   <>
-      <GlStyles/>
-      <Header/>
-      <Main/>
-      <MkBoxBtn/>
-      <Instr/>
-      <Carousel/>
-      <Footer/>
+    <GlStyles />
+    <Header />
+    <Main />
+    <MkBoxBtn />
+    <Instr />
+    <AccordionFaq />
+    <Carousel />
+    <Footer />
   </>
 )
 

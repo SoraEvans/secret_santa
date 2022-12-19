20f99f6cc66c833320c709b0e4cfc4cc14555b5b
Alexander Potikhonov <pas1996@mail.ru>
2022-12-19 17:47:30 +0300

    small fix

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 import Cont from './style_cont'
 
 const HeaderEl = styled.header`
   background: #c4c5f6;
+  position: relative;
+  z-index: 2;
 `
 
 const Wrapper = styled.div`
